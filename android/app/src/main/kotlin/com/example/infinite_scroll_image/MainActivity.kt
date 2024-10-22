47c6158d9a2bf1b98cbd4e22254eca9a3e0463c7
Si Yujun <siuj0810@gmail.com>
2024-10-22 23:16:19 +0900

    new project start

---
+++ android/app/src/main/kotlin/com/example/infinite_scroll_image/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.infinite_scroll_image
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:infinite_scroll_image/root.dart';
+
+void main() {
+  runApp(const MyApp());
+}
